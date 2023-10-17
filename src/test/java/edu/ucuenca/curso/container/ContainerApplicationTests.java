78713868e696712e4245dfeb5460e93df2f80efb
minerba <minerba@Ubuntu2204.minerba.virtualbox.org>
2023-10-17 09:35:35 -0500

    configuracion inicial

---
+++ src/test/java/edu/ucuenca/curso/container/ContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucuenca.curso.container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
